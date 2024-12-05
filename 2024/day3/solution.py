221ecdd868b3d89874d5dd72b1f2f56f7e82beed
Gioele Buriani <gioele.buriani@gmail.com>
2024-12-05 12:02:45 +0100

    Setup day 3

---
+++ 2024/day3/solution.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+
+def parse_input_file(file_path):
+    '''
+    Function to read and parse the content of the input file
+
+    Parameters:
+    file_path (str): The path to the input file
+
+    Returns:
+    numbers (np.array): A numpy array with the numbers from the input file
+    '''
+
+    with open(file_path, 'r') as file:
+
+        # Initialize the list to store the numbers
+        numbers_list = []
+
+        # Read the file line by line
+        for line in file:
+
+            # Split the line by the spaces
+            numbers = line.split(' ')
+
+            # Convert the numbers to integers
+            numbers = np.array([int(number) for number in numbers])
+
+            # Append the numbers to the list
+            numbers_list.append(numbers)
+
+    return numbers_list
+
+
+# Main function
+def main():
+
+    # Define the path to the input file
+    file_path = '2024/day2/input.txt'
+
+    # Read and parse the content of the file
+    numbers = parse_input_file(file_path)
+
+    
+
+
+if __name__ == '__main__':
+    main()
