d4c2b4a54ecf7bd6804041e3a6226271937f4b55
Gioele Buriani <gburiani@lely.com>
2024-12-13 11:35:15 +0100

    debugging day 6

--- 2024/day6/solution.py
+++ 2024/day6/solution.py
@@ -2,6 +2,15 @@ import numpy as np
 
 
 def parse_input_file(file_path):
+    """
+    Parse the input file and return a numpy array with the map.
+
+    Parameters:
+    file_path (str): The path to the input file.
+
+    Returns:
+    map (numpy array): The map as a numpy array.
+    """
 
     with open(file_path, 'r') as file:
 
@@ -12,9 +21,22 @@ def parse_input_file(file_path):
 
 
 def calculate_path(map):
+    """
+    Calculate the path of the robot and the number of obstacles.
+
+    Parameters:
+    map (numpy array): The map as a numpy array.
+
+    Returns:
+    steps (int): The number of steps the robot took.
+    n_obstacles (int): The number of obstacles the robot encountered.
+    """
 
     states = []
     obstacles = []
+    no_obstacles = []
+
+    obstacles_b = []
     
     position = np.array(np.where(map == '^')).flatten()
     direction = 1
@@ -26,8 +48,6 @@ def calculate_path(map):
         state_copy = state.copy()
         map_copy = map.copy()
 
-        # if not np.any([np.array_equal(state[0:2], st[0:2]) for st in states]):
-        #     states.append(state.copy())
         states.append(state.copy())
 
         if check_end(map, state):
@@ -38,26 +58,53 @@ def calculate_path(map):
         map_copy[obstacle[0], obstacle[1]] = '#'
 
         if find_loop(map_copy, state, loop_states):
-            obstacles.append(obstacle.copy())
+            if not np.any([np.array_equal(obstacle, ob) for ob in no_obstacles]):
+                obstacles_b.append(obstacle.copy())
+            if not np.any([np.array_equal(obstacle, st[0:2]) for st in states]):
+                obstacles.append(obstacle.copy())
+        else:
+            no_obstacles.append(obstacle.copy())
         
         if check_obstacle(map, state):
             if state[2] == 4:
                 state[2] = 1
             else:
                 state[2] += 1
+            if check_obstacle(map, state):
+                if state[2] == 4:
+                    state[2] = 1
+                else:
+                    state[2] += 1
 
         state = move(state)
 
+    # Find the element that is in obstacles_b but not in obstacles
+    for ob in obstacles_b:
+        if not np.any([np.array_equal(ob, ob_b) for ob_b in obstacles]):
+
     steps = len(np.unique(np.vstack(states)[:,0:2], axis=0))    
     n_obstacles = len(np.unique(np.vstack(obstacles), axis=0))
+    n_obstacles_b = len(np.unique(np.vstack(obstacles_b), axis=0))
+    print(n_obstacles_b)
 
-    if initial_state[0:2] in obstacle:
+    if np.any([np.array_equal(initial_state[0:2], ob) for ob in obstacles]):
         n_obstacles -= 1
 
     return steps, n_obstacles
 
 
 def find_loop(map, state, loop_states):
+    """
+    Find a loop in the path of the robot.
+    
+    Parameters:
+    map (numpy array): The map as a numpy array.
+    state (numpy array): The current state of the robot.
+    loop_states (list): A list of states the robot has been in.
+    
+    Returns:
+    bool: True if a loop is found, False otherwise.
+    """
 
     if not np.any([np.array_equal(state, st) for st in loop_states]):
         loop_states.append(state.copy())
@@ -109,6 +156,16 @@ def find_loop(map, state, loop_states):
 
 
 def check_end(map, state):
+    """
+    Check if the robot has reached the end of the map.
+    
+    Parameters:
+    map (numpy array): The map as a numpy array.
+    state (numpy array): The current state of the robot.
+    
+    Returns:
+    bool: True if the robot has reached the end of the map, False otherwise.
+    """
     
     limit = map.shape
 
@@ -125,6 +182,16 @@ def check_end(map, state):
 
 
 def check_obstacle(map, state):
+    """
+    Check if the robot has encountered an obstacle.
+
+    Parameters:
+    map (numpy array): The map as a numpy array.
+    state (numpy array): The current state of the robot.
+
+    Returns:
+    bool: True if the robot has encountered an obstacle, False otherwise
+    """
 
     if state[2] == 1:
         if map[state[0] - 1, state[1]] == '#':
@@ -143,6 +210,15 @@ def check_obstacle(map, state):
 
 
 def move(state):
+    """
+    Move the robot to the next position.
+
+    Parameters:
+    state (numpy array): The current state of the robot.
+
+    Returns:
+    state (numpy array): The new state of the robot.
+    """
 
     if state[2] == 1:
         state[0] -= 1
