6659cd2151bedc6be332d7635ba755a4427f450c
Gioele Buriani <gburiani@lely.com>
2024-12-13 16:31:11 +0100

    Started day 7

--- 2024/day7/solution.py
+++ 2024/day7/solution.py
@@ -5,14 +5,29 @@ def parse_input_file(file_path):
 
     with open(file_path, 'r') as file:
 
-        lines = file.readlines()
+        equations = []
 
-    return True
+        for line in file:
+
+            equation = np.array(line.split())
+            equations.append(equation)
+
+    return equations
+
+
+def check_feasibility(equation):
+
+    result = equation[0]
+    terms = equation[1:]
 
 
 def main():
 
-    file_path = '2024/day6/input.txt'
+    file_path = '2024/day7/test_input.txt'
+
+    equations = parse_input_file(file_path)
+
+    print(equations)
 
     
 
