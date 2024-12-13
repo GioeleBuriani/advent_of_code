10d5616a34dd42c42edc74fc4b148bdcb1e6af67
Gioele Buriani <gburiani@lely.com>
2024-12-13 16:12:01 +0100

    Setup day 7

---
+++ 2024/day7/solution.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def parse_input_file(file_path):
+
+    with open(file_path, 'r') as file:
+
+        lines = file.readlines()
+
+    return True
+
+
+def main():
+
+    file_path = '2024/day6/input.txt'
+
+    
+
+
+if __name__ == '__main__':
+    main()
